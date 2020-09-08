d3a196da80464ca66f4018fd02a82ffc975e23ec
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-08 17:09:48 +0900

    configured webpack to accept css and images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-// import './style.css'
+import './style.css';
+import Tamales from './images/tamales.jpg';
 
+// function myComponent() {
+//   const el = document.getElementById('content')
+
+//   const image = new Image();
+//   image.src = Tamales;
+
+//   el.appendChild(image);
+
+//   return el
+// }
+
+// document.body.appendChild(myComponent());
 console.log('yo dis crazy');
